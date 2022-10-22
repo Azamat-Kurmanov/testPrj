4acf4c4e06008038108a93ca8b01e394e84d6eb0
azamat <a.kurmanov@csi.kz>
2022-10-22 16:45:04 +0600

    All files

---
+++ src/main/java/com/geekbrain/homework/ShowProducts.java
@@ -0,0 +1,48 @@
+package com.geekbrain.homework;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowProducts extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(ShowProducts.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Product> productList = getProductList();
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().printf("<html><body>");
+        for (int i = 0; i < productList.size(); i++) {
+            resp.getWriter().printf("<h3>Id: "+productList.get(i).getId()+" Title: "+productList.get(i).getTitle()+" Cost: "+productList.get(i).getCost()+" </h3>");
+        }
+        resp.getWriter().printf("</html></body>");
+    }
+
+    @Override
+    public void init()throws ServletException{
+        logger.debug("Init");
+    }
+
+    private List<Product> getProductList(){
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product(1, "Apple", 200));
+        productList.add(new Product(1, "Orange", 200));
+        productList.add(new Product(1, "Bannana", 200));
+        productList.add(new Product(1, "Grape", 200));
+        productList.add(new Product(1, "Pineapple", 200));
+        productList.add(new Product(1, "Melon", 200));
+        productList.add(new Product(1, "Watermelon", 200));
+        productList.add(new Product(1, "Peach", 200));
+        productList.add(new Product(1, "Apricot", 200));
+        productList.add(new Product(1, "Mandarin", 200));
+        productList.add(new Product(1, "Pear", 200));
+        return productList;
+    }
+}
